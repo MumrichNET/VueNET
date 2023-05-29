6d5f65da00cfb2a353bcade4eb05571a83ea4fd2
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-05-29 13:30:20 +0000

    add unocss.

--- vue-net/vite.config.ts
+++ vue-net/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
     port: 3000,
     strictPort: true
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), UnoCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
